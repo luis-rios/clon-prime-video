c24632be588058238a772b9ac4f59109a39e8c35
salomon1102 <acaru.211@gmail.com>
2021-03-12 21:24:26 -0600

    salomon

---
+++ src/Pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Carousel from '../Components/Carousel';
+import Cirius from '../Img/01.jpg';
+import '../Styles/Home.css';
+
+
+ export default function home(){
+    return(
+        <div className="contenedor_inicio">
+
+            <Carousel/>
+
+            <section>
+
+                <div >
+                  
+                </div>
+
+
+            </section>
+
+            </div>
+        
+    )
+}
