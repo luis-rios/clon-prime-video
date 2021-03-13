9afd0abb82c4b8df02a00243d62043ac173e9c85
luis-rios <vvrhz11@gmail.com>
2021-03-12 20:24:17 -0600

    v1-Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch, faUserCircle} from '@fortawesome/free-solid-svg-icons';
+import '../Styles/Header.css'
 export default function Header(){
     return (
-        <div>
-            soy header
+        <div className="Header-Container">
+            <div className="Logo">
+                
+            </div>
+            <div className="Menu">
+                <ul>
+                    <li> <a href="#" className="item"> Inicio     </a>  </li>
+                    <li> <a href="#" className="item"> Tienda     </a>  </li>
+                    <li> <a href="#" className="item"> Canales    </a>  </li>
+                    <li> <a href="#" className="item"> Categorias
+                       {/*  <ul>
+                            <li>Series</li>
+                            <li>Peliculas</li>
+                            <li>Infantil</li>
+                            <li>Se incluye con prime</li>
+                            <li>Disponible para descargar</li>
+                        </ul> */}
+                        </a>
+                    </li>
+                    <li> <a href="#" className="item"> Mi espacio </a></li>
+                </ul>
+            </div>
+            <div className="Busqueda">
+                <div className="Search-container">
+                    <FontAwesomeIcon className="btn"icon={faSearch}/>
+                    <input type="text" placeholder="Buscar"/>
+                    <div className="i">
+                        <FontAwesomeIcon className="icono-avatar" icon={faUserCircle}/>
+                    </div>
+                    <p>Luis</p>
+                </div>
+                
+                
+            </div>
+            
+           
         </div>
     )
 }
