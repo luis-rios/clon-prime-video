4af886289d4a2e35de8c22d5443126214c8f3478
luis-rios <vvrhz11@gmail.com>
2021-03-12 21:46:36 -0600

    merge-con-master

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-import Home from './Pages/Home';
-=======
 import React from 'react';
 import Home from './Pages/Home'
 
->>>>>>> salomon
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home/>
-<<<<<<< HEAD
-=======
-  
->>>>>>> salomon
     </div>
   );
 }
